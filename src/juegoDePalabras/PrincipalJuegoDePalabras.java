af3789cb97743c188ca9baf216ea9945840e52b6
QuickScope-san <kevin.cancimanse@correounivalle.edu.co>
2021-08-31 12:00:24 -0500

    Se quita mensajes de prueba

--- src/juegoDePalabras/PrincipalJuegoDePalabras.java
+++ src/juegoDePalabras/PrincipalJuegoDePalabras.java
@@ -7,8 +7,7 @@ public class PrincipalJuegoDePalabras {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		JOptionPane.showMessageDialog(null, "Mensaje de prueba");
-		JOptionPane.showMessageDialog(null, "Mensaje de prueba 2");
+		
 
 	}
 
