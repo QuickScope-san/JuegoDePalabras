8d3fbda23ee88fdd459f652371e02a17ccb7657f
Santi4go32 <quierocarne70@gmail.com>
2021-08-31 11:31:46 -0500

    Agregado ArchivosTexto y BufferedWriter and BufferedReader

--- src/juegoDePalabras/ControlJuegoDePalabras.java
+++ src/juegoDePalabras/ControlJuegoDePalabras.java
@@ -1,5 +1,17 @@
 package juegoDePalabras;
 
 public class ControlJuegoDePalabras {
-
+	private GestorDeArchivosJuegoDePalabras gestorArchivo;
+	private int fallos, serie, estado;
+	private boolean ganar, perder;
+	
+	public ControlJuegoDePalabras() {
+		gestorArchivo = new GestorDeArchivosJuegoDePalabras();
+		fallos = 0;
+		serie = 1;
+	}
+	
+	public void pasarNivel() {
+		
+	}
 }
