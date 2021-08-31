074930d7bd172ca8e4c437798394cfca32e07479
QuickScope-san <kevin.cancimanse@correounivalle.edu.co>
2021-08-31 11:14:43 -0500

    Mensaje de prueba (Main)

--- src/juegoDePalabras/PrincipalJuegoDePalabras.java
+++ src/juegoDePalabras/PrincipalJuegoDePalabras.java
@@ -1,9 +1,13 @@
 package juegoDePalabras;
 
+import javax.swing.JOptionPane;
+
 public class PrincipalJuegoDePalabras {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		JOptionPane.showConfirmDialog(null, "Mensaje de prueba");
 
 	}
 
