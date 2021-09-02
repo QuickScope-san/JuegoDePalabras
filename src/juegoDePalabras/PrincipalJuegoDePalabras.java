fc7d67e5f5c7d3b5113bb73356c5eaacddd87b8d
QuickScope-san <kevin.cancimanse@correounivalle.edu.co>
2021-09-01 22:46:24 -0500

    Avance en Gui

--- src/juegoDePalabras/PrincipalJuegoDePalabras.java
+++ src/juegoDePalabras/PrincipalJuegoDePalabras.java
@@ -1,12 +1,36 @@
 package juegoDePalabras;
 
+import java.awt.EventQueue;
+
 import javax.swing.JOptionPane;
+import javax.swing.UIManager;
 
 public class PrincipalJuegoDePalabras {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
+		try {
+			
+			String className = UIManager.getCrossPlatformLookAndFeelClassName();
+			UIManager.setLookAndFeel(className);
+			
+		} catch (Exception e) {
+			
+			JOptionPane.showMessageDialog(null, "Error en la maquina virtual, debes re-instalar el IDE");
+			
+		}
+		
+		EventQueue.invokeLater(new Runnable() {
+
+			@Override
+			public void run() {
+				
+				GuiJuegoDePalabras interfazDeJuego = new GuiJuegoDePalabras();
+				
+			}
+			
+			
+		});
 		
 
 	}
